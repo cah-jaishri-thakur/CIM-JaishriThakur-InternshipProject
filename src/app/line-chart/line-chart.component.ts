ce325f5f4bf7d38db4a50a8d4261605ed4212a97
cah-jaishri-thakur <jaishri.thakur@caridnalhealth.com>
2022-07-05 09:25:06 -0400

    added logic to the graph

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -22,6 +22,13 @@ export class LineChartComponent implements AfterViewInit  {
   data : any [];
   jsonDataResult: Transactions [] = [];
   jsonFile = './assets/json/jaishri_data_minmax.json';
+  changeQuantityArray : any[] =[];
+   dates : any []= [];
+   Dispensequantity : any [] = [];
+   Reversalquantity : any [] = [];
+   Invoicequantity : any [] = [];
+   onHandQuantity : any [] =[];
+  finalQuantites : { date: Date, type: number, quant: number }[] =[];
   constructor(private Http: HttpClient) {
     this.data = [];
   }
@@ -30,7 +37,10 @@ export class LineChartComponent implements AfterViewInit  {
 
   ngOnInit(): void {
     this.createChart();
+    //this.changeInQuantity();
     this.populateAxes();
+    this.changeInQuantity2();
+    //this.changeInQuantity();
   }
   ngAfterViewInit(): void {
 
@@ -46,14 +56,83 @@ export class LineChartComponent implements AfterViewInit  {
       return 'other';
     }
   }
-  populateAxes(){
+
+  changeInQuantity2(){ // put transaction date & quantity in hashmap and plot backwards
+    // Todo Step 1: Create an object array that takes in the transaction_type, quantity, and date X
+    //      Step 2: Sort the array according the date in descending order
+    //      Step 3: Put it through a for loop and adding conditional statements to figure out the transaction types
+    //      Step 4: Based of the transaction type, increment or decrement onHand value and push into an array
+    //      Step 5: Reverse the array so it will plot the points accordingly
+    var currentOnHand = 274;
+
+    this.Http.get('../../assets/json/jaishri_data_minmax.json', {responseType : 'text'})
+      .subscribe( resp => {
+        this.jsonDataResult = JSON.parse(resp);
+        this.jsonDataResult.forEach(result =>{
+          let obj = {date: result.transaction_date,type: result.transaction_type, quant: result.quantity }
+          this.finalQuantites.push(obj);
+          this.finalQuantites.sort().reverse();
+        })
+        console.log('finalQuant array: '+this.finalQuantites);
+       for(let i=0; i < this.finalQuantites.length; i++){
+         let obj2 = this.finalQuantites[i];
+         switch(obj2.type){
+           case 1:
+             currentOnHand += obj2.quant;
+             this.changeQuantityArray.push(currentOnHand);
+             this.dates.push(obj2.date);
+             break;
+           case 0:
+             currentOnHand -= obj2.quant;
+             this.changeQuantityArray.push(currentOnHand);
+             this.dates.push(obj2.date);
+             break;
+           case 3:
+             currentOnHand -= obj2.quant;
+             this.changeQuantityArray.push(currentOnHand);
+             this.dates.push(obj2.date);
+             break;
+         }
+       }
+       this.dates.reverse();
+       console.log('reversal:' + this.Reversalquantity);
+       console.log('dispense:' + this.Dispensequantity);
+       console.log('invoice:' + this.Invoicequantity);
+        console.log('dates:' + this.dates);
+
+        //this.chart.data.labels = this.dates;
+        this.chart.data.datasets[0].data = this.changeQuantityArray.reverse();
+        //this.chart.data.datasets[1].data = this.Reversalquantity.reverse();
+        //this.chart.data.datasets[2].data =  this.Invoicequantity.reverse();
+        this.chart.update();
+
+      })
+  }
+
+  changeInQuantity(){
+    this.Http.get('../../assets/json/jaishri_data_minmax.json', {responseType : 'text'})
+      .subscribe( resp => {
+        this.jsonDataResult = JSON.parse(resp);
+        const sortedActivities = this.jsonDataResult.sort((a, b) => {
+          console.log("a: " + a.transaction_date);
+          console.log("b: " + b.transaction_date);
+          //console.log("a value of: " + a.transaction_date.valueOf());
+          //console.log("b value of: " + b.transaction_date.valueOf());
+         // console.log("sorted: "+ (b.transaction_date.valueOf() - a.transaction_date.valueOf()))
+          return b.transaction_date.valueOf() - a.transaction_date.valueOf()});
+       // this.chart.data.labels = sortedActivities;
+
+      })
+  }
+  populateAxes(){ // Todo: clean the method/ pull out reusable code
     const Dispensedates : any []= [];
     const Dispensequantity : any [] = [];
     const Reversaldates : any []= [];
     const Reversalquantity : any [] = [];
     const Invoicedates : any [] = [];
     const Invoicequantity : any [] = [];
-    const transTypes : any [] = [];
+    const onHandQuantity : any [] =[];
+    const transTypes : any [] = []; // Todo: remove unused array/method
     let dispenses : any [] = [];
     let reversals : any [] = [];
     let invoices : any [] = [];
@@ -83,22 +162,24 @@ export class LineChartComponent implements AfterViewInit  {
        minQuantity.push(result.MIN_QTY);
        transTypes.push(transTypeString);
      })
-     invoices.forEach(result => {
+     invoices.forEach(result => { // ToDo: fix the invoicesQuantity
        let transTypeString = this.differentTransactionTypes(result.transaction_type);
        var date = result.transaction_date;
        Invoicedates.push(date);
-       Invoicequantity.push(result.quantity);
+       Invoicequantity.push(result.quantity * result.mif_package_qty);
        maxQuantity.push(result.max_qty);
        minQuantity.push(result.MIN_QTY);
+       onHandQuantity.push(result.on_hand);
        transTypes.push(transTypeString);
      })
 
-     this.chart.data.labels = Dispensedates;
-     this.chart.data.datasets[0].data = Dispensequantity;
-     this.chart.data.datasets[1].data = Reversalquantity;
-     this.chart.data.datasets[2].data = Invoicequantity;
+      this.chart.data.labels = Dispensedates.reverse();
+     //this.chart.data.datasets[0].data = Dispensequantity;
+     //this.chart.data.datasets[1].data = Reversalquantity;
+     //this.chart.data.datasets[2].data = Invoicequantity;
      this.chart.data.datasets[3].data = maxQuantity;
      this.chart.data.datasets[4].data = minQuantity;
+     //this.chart.data.datasets[5].data = onHandQuantity;
      //this.chart.data.datasets[0].label = transTypes;
      this.chart.update();
    })
@@ -112,35 +193,55 @@ export class LineChartComponent implements AfterViewInit  {
         datasets: [{
           label: 'Dispense',
           data: [],
-          fill: false
+          fill: false,
+
         },
          {
           label: 'Reversal',
           data: [],
           fill: false,
            backgroundColor:'#4dc9f6',
+           showLine: true,
         },
           {
             label: 'Invoices',
             data: [],
             fill: false,
             backgroundColor:'#00a950',
+
           },
           {
             label: 'Max',
             data: [],
             fill: false,
+            borderColor:'#acc236',
             backgroundColor:'#acc236',
           },
           {
             label: 'Min',
             data: [],
             fill: false,
+            borderColor:'#f67019',
             backgroundColor:'#f67019',
+          },
+          {
+            label: 'OnHand',
+            data: [],
+            fill: false,
+            borderColor:'#1aafa2',
+            backgroundColor:'#1aafa2',
           }]
       },
       options: {
         plugins:{
+          tooltip: {
+            callbacks: {
+              label: function (tooltipItem: any) {
+                console.log(tooltipItem)
+                return tooltipItem
+              }
+            }
+          },
           title:{
             display: true,
             text: 'Chart Title',
@@ -150,10 +251,8 @@ export class LineChartComponent implements AfterViewInit  {
         scales: {
           x: {
               display: true,
-            type: 'time',
-            time: {
-              unit: 'month'
-            },
+              //reverse: true,
+
             title: {
               display: true,
               text: 'date',
